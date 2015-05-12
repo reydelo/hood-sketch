ebc65b9fb7cf6bd2d0e33ad95d3b209167e8c950
Finnegan Hewitt <fbhewitt@gmail.com>
2015-05-12 10:45:19 -0600

    some charts

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -52,6 +52,64 @@ $(function(){
         }
       }
     });
-
   });
+
+
+  //GOOGLE CHARTS
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+                   'width':400,
+                   'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
+drawChart()
+
+
+
+google.setOnLoadCallback(drawChart);
+     function drawChart() {
+       var data = google.visualization.arrayToDataTable([
+         [ '', 'Sales', 'Expenses', 'Profit'],
+         [ '' ,1000, 400, 200],
+
+       ]);
+
+       var options = {
+         chart: {
+           title: 'Company Performance',
+           subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+         }
+       };
+
+       var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+       chart.draw(data, options);
+     }
+
+
+
+
+//on ready
 });
