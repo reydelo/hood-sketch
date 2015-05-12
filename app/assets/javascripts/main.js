34cadf142bd27975cab70c598f5234512595a80e
Reyna DeLoge <reyna.deloge@gmail.com>
2015-05-12 09:28:20 -0600

    edit js hash for non arrays

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -21,23 +21,23 @@ $(function(){
   $("div").on('click', 'a', function(){
     var city = $(".input-city").val();
     var state = $(".input-state").val();
-    var hoodname = $(this).text();
     var hood = $(this).text();
-    if (/\s/.test(hood)) {
-      hood = hood.replace(/ /g, "");
-    }
     $.ajax({
       url: "/hood",
       data: {"city": city, "state": state, "hood": hood}
     }).done(function(data) {
       console.log(data);
       $('.hood').children().remove();
-      $('.hood').append('<h3>' + hoodname + ' of ' + city + ', ' + state + '</h3>');
+      $('.hood').append('<h3>' + hood + ' of ' + city + ', ' + state + '</h3>');
       // hood characteristics of people
       for(var i = 0; i < data.length; i++){
         $(".hood").append("<h4>" + data[i]['type'] + "</h4>");
+        if (Array.isArray(data[i]['characteristic'])) {
         for(var f = 0; f < data[i]['characteristic'].length; f++){
-          $(".hood").append("<ol>" + data[i]['characteristic'][f] + "</ol>");
+          $(".hood").append("<li>" + data[i]['characteristic'][f] + "</li>");
+        }
+        } else {
+          $('.hood').append('<li>' + data[i]['characteristic'] + '</li>');
         }
       }
     });
